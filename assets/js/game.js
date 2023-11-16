b45958e0b87422928cc3d521f6df5ead76500001
Francisco Mancuello <franmavazq@gmail.com>
2023-11-15 21:52:12 -0300

    Add underscore.js library and create shuffled deck of cards

---
+++ assets/js/game.js
@@ -0,0 +1,28 @@
+/**
+ * C = Clubs
+ * 2D = Two of Diamonds
+ * 2H = Two of Hearts
+ * 2S = Two of Spades
+ */
+
+let deck = [];
+const types = ['C', 'D', 'H', 'S'];
+const specials = ['A', 'J', 'Q', 'K'];
+
+const createDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (let type of types) {
+            deck.push(i + type);
+        }
+    }
+
+    for (let type of types) {
+        for (let special of specials) {
+            deck.push(special + type);
+        }
+    }
+
+    deck = _.shuffle(deck);
+};
+
+createDeck();
